40867fbdd8474264d80d872c29d584f8ef8ca932
Albdarion <michail.legczylin@gmail.com>
2024-06-12 21:32:26 +0200

    Project's configuration updated

---
+++ _sql/delete from everything.sql
@@ -0,0 +1,8 @@
+DELETE FROM DimCondition;
+DELETE FROM DimCountry;
+--DELETE FROM DimDate;
+DELETE FROM DimEngine;
+DELETE FROM DimPlaneModel;
+
+DELETE FROM FactPlaneCrash;
+DELETE FROM FactPlaneOffer;
